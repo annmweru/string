abdaebbe993dd439545d882099ed581081dc26d4
annmweru <annmweru9@gmail.com>
2020-03-14 10:14:40 -0700

    string

---
+++ Javastring/src/javastring/Javastring.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javastring;
+
+/**
+ *
+ * @author ANN
+ */
+public class Javastring {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        String myName = "ann mweru";
+        String myAge= "is";
+        int myAgeis= 28;
+        double mygross= 28.99999;
+        System.out.println("myAge+ myName+ myAgeis");
+        System.out.println("mygross");
+        
+        
+        // TODO code application logic here
+    }
+    
+}
